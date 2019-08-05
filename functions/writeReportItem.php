934c44a275c61699d6e20d514732e7ee7c243400
gaelanlloyd <gaelanlloyd@gmail.com>
2019-08-05 10:41:24 -0700

    Improve writeReportItem

--- functions/writeReportItem.php
+++ functions/writeReportItem.php
@@ -1,13 +1,26 @@
 <?php
 
-function writeReportItem($chartTitle, $chartType, $chartData, $chartSeriesLabels, $chartAxisLabels, $chartCaption) {
+function writeReportItem( $args ) {
+
+	/**
+	 * Write the report item to the screen using the data passed to it.
+	 *
+	 * @param string $title         Chart title.
+	 * @param string $caption       Chart caption.
+	 * @param string $type          Chart type ('line', default, or 'bar').
+	 * @param array  $data          Array with chart data.
+	 * @param array  $labels_series Array with chart series labels.
+	 * @param array  $labels_axis   Array with x-axis labels.
+	 */
+
+	extract( $args );
 
 	ob_start();
 
 	// generate a unique name for the canvas
 
 	// remove any html tags that may be in the chart title
-	$canvasName = strip_tags($chartTitle);
+	$canvasName = strip_tags($title);
 
 	// remove any special characters that may remain
 	$canvasName = preg_replace('/[^A-Za-z0-9\-]/', '', $canvasName);
@@ -17,11 +30,11 @@ function writeReportItem($chartTitle, $chartType, $chartData, $chartSeriesLabels
 	$canvasName = $canvasName . '-' . rand(1000,9999);		// add random numbers
 	$canvasName = $canvasName . '-' . uniqid();				// add a uniqid to help avoid collisions
 
-	?><h2 class="chartTitle"><?php echo $chartTitle; ?></h2><?php
+	?><h2 class="chartTitle"><?php echo $title; ?></h2><?php
 
 	// if there is a report caption, write it
-	if ( !empty($chartCaption) ) {
-		?><p><?php echo $chartCaption; ?></p><?php
+	if ( !empty($caption) ) {
+		?><p><?php echo $caption; ?></p><?php
 	}
 
 	// DEBUG
@@ -29,10 +42,11 @@ function writeReportItem($chartTitle, $chartType, $chartData, $chartSeriesLabels
 
 	?>
 	<div class="reportItem">
+
 	<?php
 
-	echo writeChart($canvasName, $chartType, $chartData, $chartSeriesLabels, $chartAxisLabels);
-	echo writeTable($chartData, $chartSeriesLabels, $chartAxisLabels);
+	echo writeChart($canvasName, $type, $data, $labels_series, $labels_axis);
+	echo writeTable($data, $labels_series, $labels_axis);
 
 	?>
 	</div>
