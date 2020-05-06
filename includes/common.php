425929270d7072c6e29471f8b444da462b88437b
gaelanlloyd <gaelanlloyd@gmail.com>
2020-05-06 11:03:06 -0700

    Bump version to 1.3

--- includes/common.php
+++ includes/common.php
@@ -5,6 +5,6 @@
 // Refrain from editing this file so that updates can
 // easily be pulled down from Github during updates.
 
-$VERSION     = '1.2';
+$VERSION     = '1.3';
 $MAX_SERIES  = 7;							// Max number of series to plot
 $MAX_MONTHS  = 15;							// Max number of months to return
